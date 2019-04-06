55b80219a7695888c3cd713bca53842858ba333f
dsbis <david.bis573@gmail.com>
2019-04-06 10:11:25 -0500

    integrate Oculus library and implement basic touch controls

---
+++ LookingGlass/Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+
+        if (OVRInput.Get(OVRInput.Touch.PrimaryTouchpad))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+
+        transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+    }
+}
