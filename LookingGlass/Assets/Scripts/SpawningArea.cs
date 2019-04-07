a02d89b0ae1fb370036a50087d6f80de4f95bb2e
Colin Willenborg <colinjw@iastate.edu>
2019-04-06 22:22:23 -0500

    Word bank and block scripts mostly done. need to get the names to appear to user but otherwise we good.

--- LookingGlass/Assets/Scripts/SpawningArea.cs
+++ LookingGlass/Assets/Scripts/SpawningArea.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawningArea : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
     public float RateOfSpawn = 1;
-    public static int setBlue = 1;
-    public static int setGreen = 1;
-    public static int setRed = 1;
-    public static int setYellow = 1;
+    public static int setBlue = 0;
+    public static int setGreen = 0;
+    public static int setRed = 0;
+    public static int setYellow = 0;
     public static int totalSet = setBlue + setGreen + setRed + setYellow;
+    public static List<string> blueWords = new List<string>();
+    public static List<string> greenWords = new List<string>();
+    public static List<string> redWords = new List<string>();
+    public static List<string> yellowWords = new List<string>();
+
 
 
     private float nextSpawn = 0;
@@ -20,37 +26,103 @@ public class SpawningArea : MonoBehaviour
     private int totalGreen = 0;
     private int totalYellow = 0;
     private bool flag = false;
+    private bool check = false;
 
+    void Start()
+    {
+        List<int> temp = new List<int>();
+
+        int i = 0;
+        while (i < 4)
+        {
+            int randValue = Random.Range(0, 15);
+            if (!temp.Contains(randValue))
+            {
+                temp.Add(randValue);
+                i++;
+            }
+
+        }
+        for (int s = 0; s < 15; s++)
+        {
+            for (int l = 0; l < 4; l++)
+            {
+                if (l == 0)
+                {
+                    if (!WordBase.wordBank[s, temp[0]].Equals(""))
+                    {
+                        blueWords.Add(WordBase.wordBank[s, temp[0]]);
+                        print(WordBase.wordBank[s, temp[l]]);
+                    }
+                }
+                else if (l == 1)
+                {
+                    if (!WordBase.wordBank[s, temp[1]].Equals(""))
+                    {
+                        greenWords.Add(WordBase.wordBank[s, temp[1]]);
+                        print(WordBase.wordBank[s, temp[l]]);
+                    }
+                }
+                else if (l == 2)
+                {
+                    if (!WordBase.wordBank[s, temp[2]].Equals(""))
+                    {
+                        redWords.Add(WordBase.wordBank[s, temp[2]]);
+                        print(WordBase.wordBank[s, temp[l]]);
+                    }
+                }
+                else if (l == 3)
+                {
+                    if (!WordBase.wordBank[s, temp[3]].Equals(""))
+                    {
+                        yellowWords.Add(WordBase.wordBank[s, temp[3]]);
+                        print(WordBase.wordBank[s, temp[l]]);
+                    }
+                }
+            }
+        }
+
+        setGreen = greenWords.Capacity;
+        setBlue = blueWords.Capacity;
+        setRed = redWords.Capacity;
+        setYellow = yellowWords.Capacity;
+        totalSet = setBlue + setGreen + setRed + setYellow;
+        print(totalSet);
+    }
     // Update is called once per frame
     void Update()
     {
         if (Time.time > nextSpawn && total < totalSet)
         {
             nextSpawn = Time.time + RateOfSpawn;
-            int randValue = (int) Random.Range(0, 3);
-            if(randValue == 0 && totalBlue < setBlue)
+            int randValue = Random.Range(0, 4);
+            if (randValue == 0 && totalBlue < setBlue)
             {
                 ObjectToSpawn.tag = "Blue";
+                ObjectToSpawn.name = blueWords[totalBlue];
                 totalBlue++;
                 flag = true;
             }
-            else if(randValue == 1 && totalGreen < setGreen)
+            else if (randValue == 1 && totalGreen < setGreen)
             {
                 ObjectToSpawn.tag = "Green";
+                ObjectToSpawn.name = greenWords[totalGreen];
                 totalGreen++;
                 flag = true;
             }
             else if (randValue == 2 && totalRed < setRed)
             {
                 ObjectToSpawn.tag = "Red";
+                ObjectToSpawn.name = redWords[totalRed];
                 totalRed++;
                 flag = true;
             }
-            else
+            else if (randValue == 3 && totalYellow < setYellow)
             {
                 if (totalYellow < setYellow)
                 {
                     ObjectToSpawn.tag = "Yellow";
+                    ObjectToSpawn.name = yellowWords[totalYellow];
                     totalYellow++;
                 }
                 flag = true;
@@ -68,3 +140,4 @@ public class SpawningArea : MonoBehaviour
         }
     }
 }
+
--- LookingGlass/Assets/Scripts/WordBase.cs
+++ LookingGlass/Assets/Scripts/WordBase.cs
@@ -1,17 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
 using UnityEngine;
 
 public class WordBase : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static string[,] wordBank = new string[15, 65];
+
     void Start()
     {
-        
+        ReadTextFile();
     }
 
-    void ReadCSVFile()
-    {
-        StreamReader strReader = new StreamReader()
+    public void ReadTextFile() {
+        string path = "Assets/Resources/Book2.csv";
+        FileInfo sourceFile = new FileInfo(path);
+        StreamReader reader = sourceFile.OpenText();
+        string inputLine = "";
+        int rowNumber = 0, colNumber = 0;
+        while (!reader.EndOfStream) {
+            String temp = "";
+            inputLine = reader.ReadLine();
+            char[] inputLineChar = inputLine.ToCharArray();
+            for(int i = 0; i < inputLineChar.Length; i++)
+            {
+                if (inputLineChar[i] != ',')
+                {
+                    temp += inputLineChar[i];
+                }
+                else
+                {
+                    wordBank[rowNumber, colNumber] = temp;
+                    temp = "";
+                    colNumber++;
+                }
+            }
+            colNumber = 0;
+            rowNumber++;
+        }
+        reader.Close();
+        
     }
 }
