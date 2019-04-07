24a521b7aabe0e8ebedbbaf0e51370b9e2f816ea
dsbis <david.bis573@gmail.com>
2019-04-07 01:25:10 -0500

    add region feature

--- LookingGlass/Assets/Scripts/SunMotion.cs
+++ LookingGlass/Assets/Scripts/SunMotion.cs
@@ -4,25 +4,20 @@ using UnityEngine;
 
 public class SunMotion : MonoBehaviour
 {
+
     private int direction = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
        
-        transform.Translate(Vector3.down * Time.deltaTime * 75.0f * direction);
-        transform.Translate(Vector3.back * Time.deltaTime * 75.0f * direction);
+        transform.Rotate(new Vector3(-0.02f, 0.0f));
 
-        if (transform.position.y < 375.0f || transform.position.y > 1000.0f)
-        {
-            direction *= -1;
-        }
-
-        transform.Rotate(new Vector3(0.0f, 2.0f));
+        
     }
 }
